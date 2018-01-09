bd1b87def77b54cd7d9611d91f7f190bd688db41
bmagnusson <brent.magnusson@futurice.com>
2018-01-09 13:50:43 +0200

    add notes

--- copy_file.py
+++ copy_file.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import os
 import sys
 from shutil import copyfile
@@ -9,16 +8,23 @@ if len(sys.argv) > 1:
 else:
     file = 'Test.ipynb'
 
-file_path = '/home/ec2-user/ml-crash-course/{}'.format(file)
+if len(sys.argv) > 2:
+    number_of_users = sys.argv[2]
+else:
+    number_of_users = 4
 
-for i in range(1, 3):
+
+file_path = '/home/ec2-user/ml-crash-course/{}'.format(file)
+for i in range(1, number_of_users + 1):
     copy_path = '/home/user{}/{}'.format(i, file)
     try:
         os.remove(copy_path)
+        print('Deleted {}'.format(copy_path))
     except OSError:
         pass
 
     copyfile(file_path, copy_path)
+    print('Copied {} to {}'.format(file_path, copy_path))
 
 
 
