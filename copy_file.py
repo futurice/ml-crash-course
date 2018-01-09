b7e87182cea9cb812a1e45b04ece0e0f3fa552c1
bmagnusson <brent.magnusson@futurice.com>
2018-01-09 13:44:27 +0200

    initial commit

---
+++ copy_file.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+from shutil import copyfile
+
+if len(sys.argv) > 1:
+    file = sys.argv[1]
+else:
+    file = 'Test.ipynb'
+
+file_path = '/home/ec2-user/ml-crash-course/{}'.format(file)
+
+for i in range(1, 3):
+    copy_path = '/home/user{}'.format(i)
+    try:
+        os.remove(copy_path)
+    except OSError:
+        pass
+
+    copyfile(file_path, copy_path)
+
+
+
